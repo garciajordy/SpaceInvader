66c7b3450c6a4544f8578b726c5c8ce01361d209
garciajordy <jordygarcia0119@gmail.com>
2021-05-11 12:36:08 +0300

    Add movemoents to player and create player class

--- js/SceneMain.js
+++ js/SceneMain.js
@@ -28,43 +28,72 @@ class SceneMain extends Phaser.Scene {
         this.load.audio("sndExplode0", "content/sndExplode0.wav");
         this.load.audio("sndExplode1", "content/sndExplode1.wav");
         this.load.audio("sndLaser", "content/sndLaser.wav");
-    }
-
-    create() {
+      }
+      
+      create() {
+      this.anims.create({
+          key: "sprEnemy0",
+          frames: this.anims.generateFrameNumbers("sprEnemy0"),
+          frameRate: 20,
+          repeat: -1
+        });
+    
         this.anims.create({
-            key: "sprEnemy0",
-            frames: this.anims.generateFrameNumbers("sprEnemy0"),
-            frameRate: 20,
-            repeat: -1
-          });
+          key: "sprEnemy2",
+          frames: this.anims.generateFrameNumbers("sprEnemy2"),
+          frameRate: 20,
+          repeat: -1
+        });
+    
+        this.anims.create({
+          key: "sprExplosion",
+          frames: this.anims.generateFrameNumbers("sprExplosion"),
+          frameRate: 20,
+          repeat: 0
+        });
+    
+        this.anims.create({
+          key: "sprPlayer",
+          frames: this.anims.generateFrameNumbers("sprPlayer"),
+          frameRate: 20,
+          repeat: -1
+        });
       
-          this.anims.create({
-            key: "sprEnemy2",
-            frames: this.anims.generateFrameNumbers("sprEnemy2"),
-            frameRate: 20,
-            repeat: -1
-          });
-      
-          this.anims.create({
-            key: "sprExplosion",
-            frames: this.anims.generateFrameNumbers("sprExplosion"),
-            frameRate: 20,
-            repeat: 0
-          });
-      
-          this.anims.create({
-            key: "sprPlayer",
-            frames: this.anims.generateFrameNumbers("sprPlayer"),
-            frameRate: 20,
-            repeat: -1
-          });
-        
-          this.sfx = {
-            explosions: [
-              this.sound.add("sndExplode0"),
-              this.sound.add("sndExplode1")
-            ],
-            laser: this.sound.add("sndLaser")
-          };
+        this.sfx = {
+          explosions: [
+            this.sound.add("sndExplode0"),
+            this.sound.add("sndExplode1")
+          ],
+          laser: this.sound.add("sndLaser")
+        };
+          this.player = new Player(
+            this,
+            this.game.config.width * 0.5,
+            this.game.config.height * 0.5,
+            "sprPlayer"
+          );
+          this.keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
+          this.keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
+          this.keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
+          this.keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
+          this.keySpace = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+      }
+  update() {
+    
+    this.player.update();
+
+    if (this.keyW.isDown) {
+      this.player.moveUp();
     }
+    else if (this.keyS.isDown) {
+      this.player.moveDown();
+    }
+    
+    if (this.keyA.isDown) {
+      this.player.moveLeft();
+    }
+    else if (this.keyD.isDown) {
+      this.player.moveRight();
+    }
+  }
   }
