8925d97420f14727bd614fc613ca14662c7c7925
garciajordy <jordygarcia0119@gmail.com>
2021-05-13 13:22:20 +0300

    Solve webpack issue by adding the output file in config

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,7 @@ const stylesHandler = 'style-loader';
 const config = {
     entry: './src/index.js',
     output: {
+        filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
     },
     devServer: {
